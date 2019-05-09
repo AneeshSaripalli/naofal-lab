fixed run.sh script to include COM calculation on new AprilTag result using smaller AprilTag size

apriltags/scripts/move_road_to_back.py
@@ -6,8 +6,9 @@ import numpy as np
 import rot_matrix_solve as d_basis
 
 # Translation Basis Definitions
+# All sync positions are defined row wise (x, y, z)
 MAT_B1 = np.array([
-    [2.22711,	-0.226531,	-0.0671484],
+    [2.22711,	-0.226531,	-0.0671484], # x = 2.22711, y = -.226531, z = -.-0671484...
     [6.45367,  -3.57227,    0.0133328],
     [3.50909,  -2.01472, -0.0777887]
 ]).T

apriltags/scripts/unify_road_and_head.py
@@ -97,14 +97,14 @@ def get_road_data(POSE_ALL, R2B, offset, face_csv_file):
             face_csv_file.write(row)
         else:
             road_trans = np.asarray((road_row['projX'], road_row['projY'], road_row['projZ']))
-            vis_trans = np.asarray((vis_row['x'], vis_row['y'], vis_row['z']))
+            vis_trans = np.asarray((-vis_row['z'], -vis_row['x'], vis_row['y'])) # transforming OpenGL coordinates from visualize_2 script to ApriLTag coordinate system
 
             face_to_april = road_trans - vis_trans
 
             sph = calc_spherical(face_to_april)
 
             row = "%d\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n" % (int(vis_f), face_to_april[0], face_to_april[1], face_to_april[2], 
-                vis_row['x'], vis_row['y'], vis_row['z'], sph[0], sph[1], sph[2]) 
+                -vis_row['z'], -vis_row['x'], vis_row['y'], sph[0], sph[1], sph[2]) 
             face_csv_file.write(row)
 
         road_row_ptr = road_row_ptr + 1

visualize_2/Visualize_2.py
@@ -33,12 +33,6 @@ import rotation_functions as rf
 """
 
 
-f_cam = 1000
-n_tags = 23
-no_subj = 1
-pose_all = np.empty((no_subj), dtype=object)
-mesh_all = np.empty((no_subj), dtype=object)
-suct = 0
 
 NULL_MARKER = 2222
 
@@ -56,7 +50,14 @@ def main():
     process_back_data(back_csv_path)
 
 
-def process_back_data(file):
+def process_back_data(file):    
+    f_cam = 1000
+    n_tags = 23
+    no_subj = 1
+    pose_all = np.empty((no_subj), dtype=object)
+    mesh_all = np.empty((no_subj), dtype=object)
+    suct = 0
+    
     print('Calculating center of mass on file', file)
     for i in np.array([1]):  # files in os.listdir(path_loc):
         file_path = file